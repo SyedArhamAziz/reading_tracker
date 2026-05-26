3b1e3ba77f35a0c72137c9959526eb389c9d2413
Bob Bytes <bytesbob@gmail.com>
2026-05-26 01:22:07 -0400

    added basic sqlite, tokenization, help and clear commands

--- reading_tracker.py
+++ reading_tracker.py
@@ -1,15 +1,58 @@
 import os
+import re
+import sqlite3
+
+CONN = sqlite3.connect("books.db")
+CURSOR = CONN.cursor()
+CURSOR.execute('''
+CREATE TABLE IF NOT EXISTS books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    author TEXT,
+    status TEXT
+                                )
+               '''
+)
+
 
 PROMPT = "tracker > "
-TOKENS = ['q']
+BUILTINS = ['q', 'help', 'add-book', 'clear']
+
+def quit():
+    exit()
+
+def help():
+    print(
+'''clear: clear the terminal
+help: prints out list of commands
+q: quit''')
+
+def add_book():
+    name = input('name: ')
+    author = input('author: ')
+    publish_date = input('publish_date: ')
+    page_number = input('number of pages: ')
 
 if __name__ == "__main__":
-    os.system('clear')
     while(1):
         print(PROMPT, end='')
         message = input()
-        if message not in TOKENS:
-            print("Command not found")
-        if message == 'q':
-            exit()
+        
+        #tokenize input
+        message = message.strip()
+        tokens = re.split(' \n\r\t', message)
+        if tokens[0] == '':
+            continue
+        if message not in BUILTINS:
+            print(f"{tokens[0]}: Command not found")
     
+        match tokens[0]:
+            case 'q':
+                quit()
+            case 'help':
+                help()
+            case 'add-book':
+                add_book()
+            case 'clear':
+                os.system('clear')
+
